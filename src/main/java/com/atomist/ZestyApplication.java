1627e6859812072c415d812a8c4e2bf363aa3741
Rod Johnson <johnsonroda@gmail.com>
2018-07-13 18:20:18 +1000

    Everyone needs encouragement to write Java

--- src/main/java/com/atomist/ZestyApplication.java
+++ src/main/java/com/atomist/ZestyApplication.java
@@ -1,3 +1,5 @@
+// You belong everywhere you are.
+
 // Our bodies and minds are capable of far more than our psyche will let us achieve.
 
 // You're good enough, you're smart enough, and doggone it, people like you.
