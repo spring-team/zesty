f4e97679e3e79fc914281f8f888fed4a2aeff65e
Rod Johnson <johnsonroda@gmail.com>
2018-07-31 11:19:44 +1000

    Everyone needs encouragement to write Java

--- src/main/java/com/atomist/ZestyApplication.java
+++ src/main/java/com/atomist/ZestyApplication.java
@@ -1,3 +1,5 @@
+// Our bodies and minds are capable of far more than our psyche will let us achieve.
+
 // Life is psychologically difficult for everybody.
 
 // Finite games are played within boundaries. Infinite games play with boundaries.
