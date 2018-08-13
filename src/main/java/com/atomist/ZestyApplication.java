eaec57b7208d5452945573612b770c5c815db155
Rod Johnson <johnsonroda@gmail.com>
2018-08-13 07:45:48 -0700

    Everyone needs encouragement to write Java

--- src/main/java/com/atomist/ZestyApplication.java
+++ src/main/java/com/atomist/ZestyApplication.java
@@ -1,3 +1,5 @@
+// You are the delta in what you do, not the things you did in the past
+
 // Finite games are played within boundaries. Infinite games play with boundaries.
 
 // I believe in you. You can do the thing!
