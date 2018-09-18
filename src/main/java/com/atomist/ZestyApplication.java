e9d9fd0eacb3b9c498426ad2433d024ce9b7893b
Rod Johnson <johnsonroda@gmail.com>
2018-09-18 18:14:22 +1000

    Everyone needs encouragement to write Java

--- src/main/java/com/atomist/ZestyApplication.java
+++ src/main/java/com/atomist/ZestyApplication.java
@@ -1,3 +1,5 @@
+// Finite games are played within boundaries. Infinite games play with boundaries.
+
 // You are the delta in what you do, not the things you did in the past
 
 // Finite games are played within boundaries. Infinite games play with boundaries.
