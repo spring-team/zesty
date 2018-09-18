c4307b54e845dcf14f9e3d18ed2619cb1b6f8cbe
Rod Johnson <johnsonroda@gmail.com>
2018-09-19 07:46:03 +1000

    Everyone needs encouragement to write Java

--- src/main/java/com/atomist/ZestyApplication.java
+++ src/main/java/com/atomist/ZestyApplication.java
@@ -1,3 +1,5 @@
+// You are the delta in what you do, not the things you did in the past
+
 // Our bodies and minds are capable of far more than our psyche will let us achieve.
 
 // I believe in you. You can do the thing!
