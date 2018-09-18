af41616465ff99c1fc11af830d205771e6dbc4be
Rod Johnson <johnsonroda@gmail.com>
2018-09-18 18:25:40 +1000

    Everyone needs encouragement to write Java

--- src/main/java/com/atomist/ZestyApplication.java
+++ src/main/java/com/atomist/ZestyApplication.java
@@ -1,3 +1,5 @@
+// Finite games are played within boundaries. Infinite games play with boundaries.
+
 // You belong everywhere you are.
 
 // Our bodies and minds are capable of far more than our psyche will let us achieve.
