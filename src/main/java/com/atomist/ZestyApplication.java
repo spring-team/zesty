765c51f89d021544cf349ffb500fe702d45e0fdd
Christian Dupuis <cd@atomist.com>
2018-09-18 22:48:46 +0200

    Everyone needs encouragement to write Java

--- src/main/java/com/atomist/ZestyApplication.java
+++ src/main/java/com/atomist/ZestyApplication.java
@@ -1,3 +1,5 @@
+// Finite games are played within boundaries. Infinite games play with boundaries.
+
 // Life is psychologically difficult for everybody.
 
 // It’s only a thought, and a thought can be changed.
